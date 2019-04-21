221424962ec6069fc72df41ca1d63d79bc09fae2
mchavez@nisum.com <maty123123>
2019-04-21 13:24:49 -0300

    Initial commit

---
+++ src/main/java/com/android/canitoapi/CanitoapiApplication.java
@@ -0,0 +1,13 @@
+package com.android.canitoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanitoapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanitoapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/android/canitoapi/CanitoapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.android.canitoapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CanitoapiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
